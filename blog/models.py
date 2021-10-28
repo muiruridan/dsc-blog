17d812583ea1ecbada9739def10ea04159d254c2
William Otieno <jimmywilliamotieno@gmail.com>
2021-10-28 21:11:21 +0300

    lesson 1 dsc

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
 
 # Create your models here.
 
 class Blog(models.Model):
-    pass
+    name = models.CharField(max_length=100)
+    content = models.TextField(null=True)
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.name} - {self.date_added.date()}"
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Blog
 
 # Create your views here.
+
+
+def home(request):
+    blogs = Blog.objects.filter(name='1st Blog')
+    q = blogs.query
+    return HttpResponse(q)
